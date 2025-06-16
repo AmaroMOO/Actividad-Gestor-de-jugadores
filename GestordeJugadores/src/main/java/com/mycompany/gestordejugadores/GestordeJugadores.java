ffb7e5497d2b72b50a17aad786e36b9d715d07e0
Amaro Morales <amaroromerocedrez@gmail.com>
2025-06-16 08:20:48 +0100

    Gestor de jugadores

---
+++ GestordeJugadores/src/main/java/com/mycompany/gestordejugadores/GestordeJugadores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestordejugadores;
+
+/**
+ *
+ * @author alumno
+ */
+public class GestordeJugadores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
